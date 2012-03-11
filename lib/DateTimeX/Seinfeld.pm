30faebf275f1ce46b26b4a775fc08cc61a180fa2
Christopher J. Madsen <perl@cjmweb.net>
2012-03-10 23:01:44 -0600

    Add docs

--- lib/DateTimeX/Seinfeld.pm
+++ lib/DateTimeX/Seinfeld.pm
@@ -29,12 +29,28 @@ our $VERSION = '0.01';
 
 #=====================================================================
 
+=attr start_date
+
+This is the DateTime of the beginning of the first period.  All events
+passed to C<find_chains> must be greater than or equal to this value.
+(required)
+
+=cut
+
 has start_date => (
   is       => 'ro',
   isa      => 'DateTime',
   coerce   => 1,
   required => 1,
 );
+
+=attr increment
+
+This is the DateTime::Duration giving the length of each period.  You
+may also pass a hashref acceptable to the DateTime::Duration
+constructor.  (required)
+
+=cut
 
 has increment => (
   is       => 'ro',
@@ -43,12 +59,75 @@ has increment => (
   required => 1,
 );
 
+=attr skip
+
+This is a CodeRef that allows you to skip specified periods.  It is
+called with one argument, the DateTime at which the period begins.  If
+the CodeRef returns true, any events taking place during this period
+are instead considered to take place in the next period.  (The CodeRef
+must not change the DateTime object it was given.)  (optional)
+
+For example, to skip Sundays:
+
+  skip => sub { shift->day_of_week == 7 }
+
+=cut
+
 has skip => (
   is       => 'ro',
   isa      => CodeRef,
 );
 
 #=====================================================================
+
+=method find_chains
+
+  $info = $seinfeld->find_chains( \@events );
+
+This calculates the Seinfeld chain from the events in C<@events>
+(which must be sorted in ascending order).  Note that you must pass an
+array reference, not a list.
+
+On return, C<$info> will contain a hashref with two keys: C<last> (the
+last chain of events found) and C<longest> (the longest chain found).
+These may be the same chain.  The value of each key is a hashref
+describing that chain with the following keys:
+
+=over
+
+=item C<start_period>
+
+The DateTime of the start of the period containg the first event of the chain.
+
+=item C<end_period>
+
+The DateTime of the start of the period where the chain broke
+(i.e. the first period that didn't contain an event).
+
+=item C<start_event>
+
+The DateTime of the first event of the chain (this is the same object
+that appeared in C<@events>, not a copy).
+
+=item C<end_period>
+
+The DateTime of the last event in the chain (this is the same object
+that appeared in C<@events>, not a copy).
+
+=item C<length>
+
+The number of periods in the chain.
+
+=item C<num_events>
+
+The number of events in the chain.  This can never be less than
+C<length>, but it can be more (if multiple events occurred in one period).
+
+=back
+
+Note:  If C<@events> is empty, then the return value will be an empty hashref.
+
+=cut
 
 sub find_chains
 {
@@ -115,5 +194,25 @@ __END__
 
   say "Longest chain: $chains->{longest}{length}";
   say "First event in longest chain: $chains->{longest}{start_event}";
+
+=head1 DESCRIPTION
+
+DateTimeX::Seinfeld calculates the maximum Seinfeld chain length from
+a sorted list of L<DateTime> objects.
+
+The term "Seinfeld chain" comes from advice attributed to comedian
+Jerry Seinfeld.  He got a large year-on-one-page calendar and marked a
+big red X on every day he wrote something.  The chain of continuous
+X's gave him a sense of accomplishment.
+(Source: L<http://lifehacker.com/281626/jerry-seinfelds-productivity-secret>)
+
+This module calculates the length of the longest such chain of
+consecutive days.  However, it generalizes the concept; instead of
+having to do something every day, you can make it every week, or every
+month, or any other period that can be defined by a
+L<DateTime::Duration>.
+
+Some definitions: B<period> is the time period during which some B<event>
+must occur.
 
 =cut
